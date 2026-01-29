854864df6a9e5ed1844b4bf5bf7beec2a06fdee3
HansonJoel <joelhanson002@gmail.com>
2026-01-29 11:46:02 +0100

    Fixed payment Issues

--- backend/src/config/paystack.js
+++ backend/src/config/paystack.js
@@ -1,8 +1,10 @@
-const dotenv = require("dotenv");
-
-dotenv.config();
-
 const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
+
+if (!PAYSTACK_SECRET_KEY) {
+  throw new Error(
+    "❌ PAYSTACK_SECRET_KEY is missing from environment variables",
+  );
+}
 
 module.exports = {
   PAYSTACK_SECRET_KEY,
